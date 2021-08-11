0d28c64eda6533422c44cf744e9ce0f8aedc1455
Kyle Huang <kyleghuang@gmail.com>
2021-08-11 18:43:56 -0400

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import 'tailwindcss/tailwind.css'
+
+export default function Home() {
+  return (
+    <div className="bg-green-600 w-screen h-screen">
+    </div>
+  )
+}
